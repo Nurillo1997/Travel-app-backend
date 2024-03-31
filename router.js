a0c714677fa38233596db0f7c9cf8f242fe15bc9
Nurillo1997 <nurilloabdurazzoqov@gmail>
2024-04-01 07:45:31 +0900

    feat: build member controllers

--- router.js
+++ router.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const memberController = require('./controllers/memberController');
 
-router.get("/", function(req, res){
-  res.send("home sahifasidasiz");
-});
 
+// member related routers
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
+
+
+
+// other routers
 router.get('/menu', (req,res)=>{
   res.send('Menu sahifasidasiz');
 });
